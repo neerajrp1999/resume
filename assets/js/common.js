34d6229499c2de8933a6489bd247f79478af8ec6
Neeraj R Prajapati <neerajrp1999@gmail.com>
2024-01-03 20:43:24 +0530

    resume

---
+++ assets/js/common.js
@@ -0,0 +1,193 @@
+$(document).ready(function() {
+
+    'use strict';
+
+    /*-----------------------------------------------------------------
+      Detect device mobile
+    -------------------------------------------------------------------*/
+	
+    var isMobile = false; 
+    if( /Android|webOS|iPhone|iPod|iPad|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        $('html').addClass('touch');
+        isMobile = true;
+    }
+    else {
+        $('html').addClass('no-touch');
+        isMobile = false;
+    }
+	
+	//IE, Edge
+	var isIE = /MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent) || /MSIE 10/i.test(navigator.userAgent) || /Edge\/\d+/.test(navigator.userAgent);
+  
+
+    /*-----------------------------------------------------------------
+      Loaded
+    -------------------------------------------------------------------*/
+
+    anime({
+        targets: 'body',
+        opacity: 1,
+        delay: 400,
+        complete: function(anim) {
+            progressBar(); //Init progress bar
+        }
+    });
+    
+    $('body, .js-img-load').imagesLoaded({ background: !0 }).always( function( instance ) {
+	    preloader(); //Init preloader
+    });
+
+    function preloader() {
+        var tl = anime.timeline({}); 
+        tl
+        .add({
+            targets: '.preloader',
+            duration: 1,
+            opacity: 1
+        })
+        .add({
+            targets: '.circle-pulse',
+            duration: 300,
+            //delay: 500,
+            opacity: 1,
+            zIndex: '-1',
+            easing: 'easeInOutQuart'
+        },'+=500')
+        .add({
+            targets: '.preloader__progress span',
+            duration: 500,
+            width: '100%',
+			easing: 'easeInOutQuart'
+        },'-=500')
+        .add({
+            targets: '.preloader',
+            duration: 500,
+            opacity: 0,
+            zIndex: '-1',
+            easing: 'easeInOutQuart'
+        });
+    };
+
+
+    /*-----------------------------------------------------------------
+      Carousel
+    -------------------------------------------------------------------*/	
+    
+	// Testimonials
+	$('.js-carousel-review').each(function() {
+		var carousel = new Swiper('.js-carousel-review', {
+            slidesPerView: 1,
+			spaceBetween: 30,
+			speed: 300,
+			grabCursor: true,
+			watchOverflow: true,
+            pagination: {
+                el: '.swiper-pagination',
+		        clickable: true
+            },
+			autoplay: {
+                delay: 5000,
+            },
+			breakpoints: {
+                580: {
+                    spaceBetween: 20
+                }
+            }
+		});
+	});
+	
+	// Clients
+	$('.js-carousel-clients').each(function() {
+		var carousel = new Swiper('.js-carousel-clients', {
+            slidesPerView: 4,
+			spaceBetween: 30,
+			//loop: true,
+			grabCursor: true,
+			watchOverflow: true,
+            pagination: {
+                el: '.swiper-pagination',
+		        clickable: true
+            },
+			breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                    spaceBetween: 0
+                },				
+                580: {
+                    slidesPerView: 2,
+                    spaceBetween: 30
+                },				
+                991: {
+                    slidesPerView: 3,
+                    spaceBetween: 30
+                }
+            }
+		});
+	});
+
+    
+	function progressBar() {
+	    $('.progress').each(function() {
+		    var ctrl = new ScrollMagic.Controller();
+		    new ScrollMagic.Scene({
+                triggerElement: '.progress',
+	            triggerHook: 'onEnter',
+	            duration: 300
+            })
+            .addTo(ctrl)
+		    .on("enter", function (e) {
+			    var progressBar = $('.progress-bar');
+                progressBar.each(function(indx){
+                    $(this).css({'width': $(this).attr('aria-valuenow') + '%', 'z-index': '2'});
+                });
+		    });
+        });
+    }
+	
+    
+	$('.js-tabs').each(function(){
+	    $('.content .tabcontent').hide();
+        $('.content .tabcontent:first').show();
+        $('.nav__item a').on('click', function () {
+            $('.nav__item a').removeClass('active');
+            $(this).addClass('active');
+            var currentTab = $(this).attr('href');
+            $('.content .tabcontent').hide();            
+            $(currentTab).show();
+            $portfolioMasonry.isotope ({
+                columnWidth: '.gallery-grid__item',
+                gutter: '.gutter-sizer',
+                isAnimated: true
+            });
+			$('.js-scroll').getNiceScroll().resize()
+            return false;
+        });
+	    
+	
+	});
+	
+	
+    /*-----------------------------------------------------------------
+      Switch categories & Filter mobile
+    -------------------------------------------------------------------*/	
+  
+    
+
+
+  
+    $portfolioMasonry.imagesLoaded().progress( function() {
+        $portfolioMasonry.isotope ({
+	        columnWidth: '.gallery-grid__item',
+            gutter: '.gutter-sizer',
+            isAnimated: true,
+	        layoutMode: 'fitRows',
+            fitRows: {
+                gutter: '.gutter-sizer'
+            }
+	    });
+    });	
+
+
+    
+}
+);
